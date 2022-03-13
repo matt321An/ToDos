f0ab6afb645621e4b90585cc4e078aa64d2b4745
matt321An <64513508+matt321An@users.noreply.github.com>
2022-03-13 15:37:05 +0200

    list todo done

--- src/app/components/list-todo/list-todo.component.ts
+++ src/app/components/list-todo/list-todo.component.ts
@@ -8,39 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodoComponent implements OnInit {
   todoCardList = [
     {
+      id:0,
       title: "School",
       important: true,
       toDoItem: [
-        {title: "WAD Homework", date: "15 Mar 2022"},
-        {title: "SE Homework", date: "16 Mar 2022"}, 
-        {title: "Project Homework", date: "18 Mar 2022"}, 
-        {title: "CP Homework", date: "19 Mar 2022"}
+        {id:0, title: "WAD Homework", date: "15 Mar 2022", important: true, check:false},
+        {id:1, title: "SE Homework", date: "16 Mar 2022", important: true, check:false}, 
+        {id:2, title: "Project Homework", date: "18 Mar 2022", important: false, check:false}, 
+        {id:3, title: "CP Homework", date: "19 Mar 2022", important: true, check:false}
       ]
     },
     {
+      id:1,
       title: "Personal",
       important: false,
       toDoItem: [
-        {title: "Go to gym", date: "13 Mar 2022", important: false},
-        {title: "Pay mortage", date: "30 Mar ", important: true}, 
+        {id:0, title: "Go to gym", date: "13 Mar 2022", important: false, check:false},
+        {id:1, title: "Pay mortage", date: "30 Mar ", important: true, check:false}, 
       ]
     },
     {
+      id:2,
       title: "Groceries",
       important: true,
       toDoItem: [
-        {title: "Rice", date: ""},
-        {title: "Pork", date: ""}, 
-        {title: "Tomatoes", date: ""}, 
-        {title: "Oranges", date: ""}, 
-        {title: "Egs", date: ""}, 
-        {title: "Sugar", date: ""}, 
+        {id:0, title: "Rice", date: "", important: true, check:false},
+        {id:1, title: "Pork", date: "", important: false, check:false}, 
+        {id:2, title: "Tomatoes", date: "", important: false, check:false}, 
+        {id:3, title: "Oranges", date: "", important: false, check:false}, 
+        {id:4, title: "Egs", date: "", important: true, check:false}, 
+        {id:5, title: "Sugar", date: "", important: true, check:false}, 
       ]      
     }
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeCheckAttribute(todoCardId: number, todoItemId: number) {
+    this.todoCardList[todoCardId].toDoItem[todoItemId].check = 
+      !this.todoCardList[todoCardId].toDoItem[todoItemId].check;
+  }
+
+  changeImportantAttribute(todoCardId: number, todoItemId: number) {
+    this.todoCardList[todoCardId].toDoItem[todoItemId].important =
+      !this.todoCardList[todoCardId].toDoItem[todoItemId].important;
   }
 
 }
